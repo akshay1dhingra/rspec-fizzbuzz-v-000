007229dc43065341cd86f49349a4983efc931a09
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-09-12 14:10:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     print "Fizz"
-  if int % 5 == 0
+  elsif int % 5 == 0
     print "Buzz"
-  if int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     print "FizzBuzz"
   else int % 4 == 0
     print "nil"
